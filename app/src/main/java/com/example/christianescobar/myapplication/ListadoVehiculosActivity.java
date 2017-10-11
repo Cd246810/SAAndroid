36b4776aaf5b2737c3f075a20dc5005b7d3cacc8
Chris <palma@live.hk>
2017-10-11 16:35:41 -0600

    se creo la lista lv en ListadoVehiculosActivity

--- app/src/main/java/com/example/christianescobar/myapplication/ListadoVehiculosActivity.java
+++ app/src/main/java/com/example/christianescobar/myapplication/ListadoVehiculosActivity.java
@@ -6,6 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ListadoVehiculosActivity extends AppCompatActivity {
 
@@ -16,14 +19,13 @@ public class ListadoVehiculosActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ArrayList<Vehiculo> category = new ArrayList<Vehiculo>();
+
+        ListView lv = (ListView) findViewById(R.id.listView);
+
+        AdapterItem adapter = new AdapterItem(this, category);
+
+        lv.setAdapter(adapter);
     }
 
 }
