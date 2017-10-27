9a2533b244cb7cbb1d7bdf1738377477ea21b663
Kevin Juárez <you@example.com>
2017-10-27 14:19:01 -0600

    Cambios de IP y PDF

--- app/src/main/java/com/example/christianescobar/myapplication/CambiarIP.java
+++ app/src/main/java/com/example/christianescobar/myapplication/CambiarIP.java
@@ -1,9 +1,11 @@
 package com.example.christianescobar.myapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CambiarIP extends AppCompatActivity {
 
@@ -15,10 +17,34 @@ public class CambiarIP extends AppCompatActivity {
 
     }
 
-    public void CambiarIP(View v){
+    public void cambiarIP(View v){
 
-
-
-        startActivity(new Intent(CambiarIP.this, MainActivity.class));
+        EditText IP=(EditText) findViewById(R.id.txt_Tarjeta);
+        String ip=IP.getText().toString().trim();
+        String ipA[]=ip.split(".");
+        boolean error=false;
+        if(ipA.length!=4){
+            error=true;
+        }else{
+            for(String i:ipA){
+                try{
+                    int num=Integer.parseInt(i);
+                    if(num<0 || num>255){
+                        error=true;
+                        break;
+                    }
+                }catch(Exception ex){
+                    error=true;
+                    break;
+                }
+            }
+        }
+        if(error){
+            IP.setTextColor(Color.RED);
+        }else{
+            IP.setTextColor(Color.BLUE);
+            V.SERVER = ip;
+            startActivity(new Intent(CambiarIP.this, MainActivity.class));
+        }
     }
 }
